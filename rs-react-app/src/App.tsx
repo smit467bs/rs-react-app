95795b497403be06115356e9ecedc5f960a31cb4
Vyacheslav Byrdaev <smit467bs@gmail.com>
2025-02-01 14:04:18 +0300

    refactor: delete unused

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Search } from './components/search/search.tsx';
-import { Card } from './components/card/Card.tsx';
 import { CardList } from './components/card-list/card-list.tsx';
 
 function App() {
