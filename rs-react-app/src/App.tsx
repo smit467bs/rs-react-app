8bb1b598e1fa77cfd249f6dd9ee4237864547d1b
Vyacheslav Byrdaev <smit467bs@gmail.com>
2025-02-01 13:54:50 +0300

    feat: create component Card

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Search } from './components/search/search.tsx';
+import { Card } from './components/card/Card.tsx';
 
 function App() {
 
   return (
     <>
       <Search />
+      <Card name={'VasiliyInfo'} />
     </>
   );
 }
---
+++ rs-react-app/src/components/card/Card.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+interface Props {
+  name: string;
+}
+
+export class Card extends Component<Props> {
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        <p>Описание для {this.props.name}</p>
+      </div>
+    );
+  }
+
+}
