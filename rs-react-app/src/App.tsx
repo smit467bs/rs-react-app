16c6ebcd691a6b6b1e3fc7b1d62354f6f048be4c
smit467bs <smit467bs@gmail.com>
2025-02-03 14:48:33 +0300

    refactor: eslint --fix

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,92 +1,92 @@
 import './App.css';
-import {Search} from './components/search/search.tsx';
-import {CardList} from './components/card-list/card-list.tsx';
-import {Component} from 'react';
+import { Search } from './components/search/search.tsx';
+import { CardList } from './components/card-list/card-list.tsx';
+import { Component } from 'react';
 
 const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=10';
 
 interface State {
-	items: Pokemon[];
-	isLoading: boolean;
-	error: string | null;
-	searchQuery: string;
-	throwError: boolean;
+  items: Pokemon[];
+  isLoading: boolean;
+  error: string | null;
+  searchQuery: string;
+  throwError: boolean;
 }
 
 interface Pokemon {
-	name: string;
-	url: string;
+  name: string;
+  url: string;
 }
 
 export class App extends Component<{ '' }, State> {
-	constructor(props: { '' }) {
-		super(props);
-		this.state = {
-			items: [],
-			isLoading: false,
-			error: null,
-			searchQuery: localStorage.getItem('searchQuery') || '',
-			throwError: false,
-		};
-	}
+  constructor(props: { '' }) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoading: false,
+      error: null,
+      searchQuery: localStorage.getItem('searchQuery') || '',
+      throwError: false,
+    };
+  }
 
-	fetchData = async (query: string) => {
-		this.setState({isLoading: true, error: null});
-		localStorage.setItem('searchQuery', query.trim().toLowerCase());
+  fetchData = async (query: string) => {
+    this.setState({ isLoading: true, error: null });
+    localStorage.setItem('searchQuery', query.trim().toLowerCase());
 
-		try {
-			const response = await fetch(API_URL);
-			if (!response.ok) throw new Error('Ошибка загрузки');
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) throw new Error('Ошибка загрузки');
 
-			const data = await response.json();
-			this.setState({
-				items: data.results.map((item) => item),
-				isLoading: false,
-			});
-		} catch (e) {
-			this.setState({
-				error: e,
-				isLoading: false,
-			});
-		}
-	};
+      const data = await response.json();
+      this.setState({
+        items: data.results.map((item) => item),
+        isLoading: false,
+      });
+    } catch (e) {
+      this.setState({
+        error: e,
+        isLoading: false,
+      });
+    }
+  };
 
-	throwTestError = () => {
-		this.setState({throwError: true});
-	};
+  throwTestError = () => {
+    this.setState({ throwError: true });
+  };
 
-	render() {
-		if (this.state.throwError) {
-			throw new Error('Тест ошибки');
-		}
+  render() {
+    if (this.state.throwError) {
+      throw new Error('Тест ошибки');
+    }
 
-		return (
-			<div className="min-h-screen  bg-gray-100 flex flex-col items-center p-4">
-				<h1 className="text-3xl font-bold text-gray-800 mb-6">
-					PokeApi search
-				</h1>
-				<div className="w-full max-w-md bg-white shadow-md rounded-lg p-4 mb-6">
-					<Search
-						onSearch={this.fetchData}
-						defaultValue={this.state.searchQuery}
-					/>
-				</div>
-				{this.state.isLoading ? (
-					<p className="text-blue-500 text-lg">Loading...</p>
-				) : null}
-				{this.state.error ? (
-					<p className="text-red-500 text-lg">{this.state.error}</p>
-				) : null}
-				<div className="w-full max-w-md">
-					<CardList items={this.state.items}/>
-				</div>
-				<button
-					className="mt-6 bg-black hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-					onClick={this.throwTestError}
-				>
-					Выкинуть ошибку
-				</button>
-			</div>
-		);
-	}
+    return (
+      <div className="min-h-screen  bg-gray-100 flex flex-col items-center p-4">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">
+          PokeApi search
+        </h1>
+        <div className="w-full max-w-md bg-white shadow-md rounded-lg p-4 mb-6">
+          <Search
+            onSearch={this.fetchData}
+            defaultValue={this.state.searchQuery}
+          />
+        </div>
+        {this.state.isLoading ? (
+          <p className="text-blue-500 text-lg">Loading...</p>
+        ) : null}
+        {this.state.error ? (
+          <p className="text-red-500 text-lg">{this.state.error}</p>
+        ) : null}
+        <div className="w-full max-w-md">
+          <CardList items={this.state.items} />
+        </div>
+        <button
+          className="mt-6 bg-black hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+          onClick={this.throwTestError}
+        >
+          Выкинуть ошибку
+        </button>
+      </div>
+    );
+  }
 }
--- rs-react-app/src/components/card/Card.tsx
+++ rs-react-app/src/components/card/Card.tsx
@@ -1,82 +1,82 @@
-import {Component} from 'react';
+import { Component } from 'react';
 
 interface Props {
-	name: string;
-	url: string;
+  name: string;
+  url: string;
 }
 
 interface State {
-	error: Error;
-	pokeInfo: Poke | null;
+  error: Error;
+  pokeInfo: Poke | null;
 }
 
 interface Poke {
-	id: string;
-	name: string;
-	weight: string;
-	height: string;
-	sprites: {
-		front_default: string | null;
-	}
+  id: string;
+  name: string;
+  weight: string;
+  height: string;
+  sprites: {
+    front_default: string | null;
+  };
 }
 
-
 export class Card extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      error: null,
+      pokeInfo: null,
+    };
+  }
 
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			error: null,
-			pokeInfo: null,
-		}
-	}
+  componentDidMount() {
+    this.fetchData();
+  }
 
-	componentDidMount() {
-		this.fetchData();
-	}
+  async fetchData() {
+    try {
+      const response = await fetch(`${this.props.url}`);
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({ pokeInfo: data });
+      } else {
+        throw new Error(`Could not fetch data from ${this.props.url}`);
+      }
+    } catch (e) {
+      throw new Error(
+        `Could not fetch data from ${(this.props.url, e.message)}`
+      );
+    }
+  }
 
-
-	async fetchData() {
-		try {
-			const response = await fetch(`${this.props.url}`)
-			if (response.ok) {
-				const data = await response.json();
-				this.setState({pokeInfo: data});
-			} else {
-				throw new Error(`Could not fetch data from ${this.props.url}`);
-			}
-		} catch (error) {
-			throw new Error(`Could not fetch data from ${this.props.url}`);
-		}
-	}
-
-
-	render() {
-
-		return (
-			<div
-				className="bg-white shadow-md rounded-lg p-2 border border-gray-200 hover:shadow-xl transition duration-300">
-				<h3 className="text-xl font-semibold text-gray-800">
-					{this.props.name}
-				</h3>
-				{
-					this.state.pokeInfo ? (
-						<div className='text-gray-700 text-sm'>
-							{this.state.pokeInfo.sprites.front_default ?
-								(<img
-									className='w-32 h-32 mx-auto mb-2 border border-gray-300 rounded-lg'
-									src={this.state.pokeInfo.sprites.front_default} alt={this.state.pokeInfo.name}/>)
-								: null}
-							<p><span className='font-medium'>Weight: </span> {this.state.pokeInfo.weight}</p>
-							<p><span className='font-medium'>Height: </span> {this.state.pokeInfo.height}</p>
-
-						</div>
-					) : (
-						<p className="text-gray-600">Loading.....</p>
-					)
-				}
-
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="bg-white shadow-md rounded-lg p-2 border border-gray-200 hover:shadow-xl transition duration-300">
+        <h3 className="text-xl font-semibold text-gray-800">
+          {this.props.name}
+        </h3>
+        {this.state.pokeInfo ? (
+          <div className="text-gray-700 text-sm">
+            {this.state.pokeInfo.sprites.front_default ? (
+              <img
+                className="w-32 h-32 mx-auto mb-2 border border-gray-300 rounded-lg"
+                src={this.state.pokeInfo.sprites.front_default}
+                alt={this.state.pokeInfo.name}
+              />
+            ) : null}
+            <p>
+              <span className="font-medium">Weight: </span>{' '}
+              {this.state.pokeInfo.weight}
+            </p>
+            <p>
+              <span className="font-medium">Height: </span>{' '}
+              {this.state.pokeInfo.height}
+            </p>
+          </div>
+        ) : (
+          <p className="text-gray-600">Loading.....</p>
+        )}
+      </div>
+    );
+  }
 }
