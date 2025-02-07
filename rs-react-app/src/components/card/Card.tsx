0b3a1c7e33f13845d7e1fcdfb0542cc9d452fa0e
Vyacheslav Byrdaev <smit467bs@gmail.com>
2025-02-07 18:20:02 +0300

    feat: card.tsx rebuild FC

--- rs-react-app/src/components/card/Card.tsx
+++ rs-react-app/src/components/card/Card.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import { Poke } from '../../interfaces';
 
 interface Props {
@@ -6,68 +6,60 @@ interface Props {
   url: string;
 }
 
-interface State {
-  error: string | null;
-  pokeInfo: Poke | null;
-}
 
-export class Card extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      error: null,
-      pokeInfo: null,
-    };
-  }
+export const Card = ({ name, url }: Props) => {
 
-  componentDidMount() {
-    this.fetchData();
-  }
 
-  async fetchData() {
+  const [pokeInfo, setPokeInfo] = useState<Poke | null>(null);
+  const fetchData = async () => {
     try {
-      const response = await fetch(`${this.props.url}`);
+      const response = await fetch(`${url}`);
       if (response.ok) {
         const data = await response.json();
-        this.setState({ pokeInfo: data });
+        setPokeInfo(data);
       } else {
-        throw new Error(`Could not fetch data from ${this.props.url}`);
+        throw new Error(`Could not fetch data from ${url}`);
       }
     } catch (error) {
       throw new Error(
-        `Could not fetch data from ${(this.props.url, String(error))}`
+        `Could not fetch data from ${(url + String(error))}`,
       );
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="bg-white shadow-md rounded-lg p-2 border border-gray-200 hover:shadow-xl transition duration-300">
-        <h3 className="text-xl font-semibold text-gray-800">
-          {this.props.name}
-        </h3>
-        {this.state.pokeInfo ? (
-          <div className="text-gray-700 text-sm">
-            {this.state.pokeInfo.sprites.front_default ? (
-              <img
-                className="w-32 h-32 mx-auto mb-2 border border-gray-300 rounded-lg"
-                src={this.state.pokeInfo.sprites.front_default}
-                alt={this.state.pokeInfo.name}
-              />
-            ) : null}
-            <p>
-              <span className="font-medium">Weight: </span>{' '}
-              {this.state.pokeInfo.weight}
-            </p>
-            <p>
-              <span className="font-medium">Height: </span>{' '}
-              {this.state.pokeInfo.height}
-            </p>
-          </div>
-        ) : (
-          <p className="text-gray-600">Loading.....</p>
-        )}
-      </div>
-    );
-  }
-}
+
+  useEffect(() => {
+
+    fetchData();
+  }, [url]);
+
+
+  return (
+    <div className="bg-white shadow-md rounded-lg p-2 border border-gray-200 hover:shadow-xl transition duration-300">
+      <h3 className="text-xl font-semibold text-gray-800">
+        {name}
+      </h3>
+      {pokeInfo ? (
+        <div className="text-gray-700 text-sm">
+          {pokeInfo.sprites.front_default ? (
+            <img
+              className="w-32 h-32 mx-auto mb-2 border border-gray-300 rounded-lg"
+              src={pokeInfo.sprites.front_default}
+              alt={pokeInfo.name}
+            />
+          ) : null}
+          <p>
+            <span className="font-medium">Weight: </span>{' '}
+            {pokeInfo.weight}
+          </p>
+          <p>
+            <span className="font-medium">Height: </span>{' '}
+            {pokeInfo.height}
+          </p>
+        </div>
+      ) : (
+        <p className="text-gray-600">Loading.....</p>
+      )}
+    </div>
+  );
+};
