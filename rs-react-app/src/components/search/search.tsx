7fcbc5e3355ba24dd4e19f1e1b87f0dc374a63ba
Vyacheslav Byrdaev <smit467bs@gmail.com>
2025-02-07 18:02:59 +0300

    feat: search.tsx rebuild FC

--- rs-react-app/src/components/search/search.tsx
+++ rs-react-app/src/components/search/search.tsx
@@ -1,48 +1,43 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 interface Props {
   onSearch: (search: string) => void;
   defaultValue: string;
 }
 
-interface State {
-  query: string;
-}
 
-export class Search extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { query: this.props.defaultValue || '' };
-  }
+export const Search = ({ onSearch, defaultValue }: Props) => {
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ query: event.target.value });
+  const [query, setQuery] = useState(defaultValue || '');
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
 
-  hangleSearch = () => {
-    if (this.state.query) {
-      this.props.onSearch(this.state.query.trim().toLowerCase());
-    } else this.props.onSearch('');
+  const handleSearch = () => {
+    if (query.length > 0) {
+      onSearch(query.trim().toLowerCase());
+    } else onSearch('');
   };
 
-  render() {
-    return (
-      <div>
-        <input
-          className="
+
+  return (
+    <div>
+      <input
+        className="
           search-input border-l-black rounded-2xl shadow-sm text-black focus:ring-red-500"
-          type="text"
-          value={this.state.query}
-          onChange={this.handleChange}
-          placeholder="Введите запрос..."
-        />
-        <button
-          className="rounded-2xl bg-black text-white hover:bg-red-400"
-          onClick={this.hangleSearch}
-        >
-          Search
-        </button>
-      </div>
-    );
-  }
-}
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Введите запрос..."
+      />
+      <button
+        className="rounded-2xl bg-black text-white hover:bg-red-400"
+        onClick={handleSearch}
+      >
+        Search
+      </button>
+    </div>
+  );
+};
