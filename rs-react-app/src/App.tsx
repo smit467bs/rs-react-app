902778cadbb0cfa81198e72531e4d57bc823a7c1
Vyacheslav Byrdaev <smit467bs@gmail.com>
2025-02-07 19:13:45 +0300

    feat: create not found page

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 
 import { Route, Routes } from 'react-router';
 import { SearchPage } from './pages/search-page/SearchPage.tsx';
+import { NotFoundPage } from './pages/not-found/not-found-page.tsx';
 
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SearchPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 
---
+++ rs-react-app/src/pages/not-found/not-found-page.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router';
+
+
+export const NotFoundPage = () => {
+  return (
+    <div className="text-center mt-10">
+      <h2 className="text-4xl font-bold text-red-600">404 - Страница не найдена</h2>
+      <p className="text-lg mt-2">Такой страницы не существует!</p>
+      <Link to="/" className="text-blue-500 mt-4 block">Вернуться на главную</Link>
+    </div>
+  );
+};
